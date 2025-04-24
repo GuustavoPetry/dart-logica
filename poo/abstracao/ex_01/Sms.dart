6ec0b5823d9563ea930ceb7ffc1e0462dc3078f8
Gustavo Petry <gustavo.petry@proway.treina>
2025-04-24 08:08:52 -0300

    erro ortográfico

--- poo/abstracao/ex_01/Sms.dart
+++ poo/abstracao/ex_01/Sms.dart
@@ -1,7 +1,7 @@
 import 'Notificacao.dart';
 
 class Sms implements Notificacao {
-  String mensagem = '"SMS enviado com sucesso!"';
+  String mensagem = "SMS enviado com sucesso!";
   
   @override
   void enviar(String mensagem) {
